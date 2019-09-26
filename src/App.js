fae024c06a0a0364948834629d567189d73c9d97
David Alexander Leigh <daleighan@gmail.com>
2019-09-26 01:09:08 -0700

    Searches will now only be executed onClick

--- src/App.js
+++ src/App.js
@@ -1,48 +1,46 @@
 import React, {useState} from 'react';
-import {useLazyQuery} from '@apollo/react-hooks';
+import {useApolloClient} from '@apollo/react-hooks';
 import LaunchList from './LaunchList';
 import {GET_LAUNCHES} from './query.js';
 
 function App() {
+  const client = useApolloClient();
+
   const [formState, updateForm] = useState({
     missionName: '',
     launchYear: '',
     rocketName: '',
   });
 
-  const [fetchLaunches, {called, loading, data}] = useLazyQuery(GET_LAUNCHES, {
-    variables: formState,
-  });
+  const [launches, updateLaunches] = useState(null);
 
   const setFormState = (field, value) =>
     updateForm({...formState, [field]: value});
 
-
   return (
     <div className="App">
-      {Object.keys(formState).map((field, idx) => (
-        <input
-          key={idx}
-          type="text"
-          value={formState[field]}
-          onChange={e => setFormState(field, e.target.value)}
-          placeholder={field}
-        />
-      ))}
-      <button onClick={() => fetchLaunches(formState)}>Search</button>
-      {!called ? (
-        <div>Please Enter A Search</div>
-      ) : (
-        <div>
-          {loading ? (
-            <div>loading...</div>
-          ) : (
-            <LaunchList
-              launchList={[...data.launchesPast, ...data.launchesUpcoming]}
-            />
-          )}
-        </div>
-      )}
+      <div>
+        {Object.keys(formState).map((field, idx) => (
+          <input
+            key={idx}
+            type="text"
+            value={formState[field]}
+            onChange={e => setFormState(field, e.target.value)}
+            placeholder={field}
+          />
+        ))}
+        <button
+          onClick={async () => {
+            const {data} = await client.query({
+              query: GET_LAUNCHES,
+              variables: formState,
+            });
+            updateLaunches([...data.launchesPast, ...data.launchesUpcoming]);
+          }}>
+          Search
+        </button>
+      </div>
+      {launches ? <LaunchList launchList={launches} /> : <div>Please Enter Your Search</div>}
     </div>
   );
 }
